b4ba8f91da866cf052315a43c84c45875413ebee
shefat2002 <shefat2002@gmail.com>
2024-04-28 22:08:26 +0600

    Worked on Cart Page

--- lib/views/cart_screen/cart_screen.dart
+++ lib/views/cart_screen/cart_screen.dart
@@ -1,10 +1,17 @@
-import 'package:flutter/material.dart';
+import 'package:emart_plus/consts/consts.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green);
+    return Container(
+      color: Colors.white,
+      child: "Cart is Empty!"
+          .text
+          .fontFamily(semibold)
+          .color(darkFontGrey)
+          .makeCentered(),
+    );
   }
 }
