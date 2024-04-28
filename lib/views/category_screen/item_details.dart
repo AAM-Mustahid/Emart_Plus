a97be9700af9bebeb54644a36357098829a3a039
shefat2002 <shefat2002@gmail.com>
2024-04-28 13:07:18 +0600

    Worked on Category Page

--- lib/views/category_screen/item_details.dart
+++ lib/views/category_screen/item_details.dart
@@ -1,4 +1,5 @@
 import 'package:emart_plus/consts/consts.dart';
+import 'package:emart_plus/widgets_common/our_button.dart';
 
 class ItemDetails extends StatelessWidget {
   final String? title;
@@ -13,6 +14,42 @@ class ItemDetails extends StatelessWidget {
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
           IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_outline))
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+            padding: const EdgeInsets.all(8),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  VxSwiper.builder(
+                    itemCount: 3,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    height: 350,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        imgFc5,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  )
+                ],
+              ),
+            ),
+          )),
+          SizedBox(
+            width: double.infinity,
+            height: 60,
+            child: ourButton(
+                color: redColor,
+                onPress: () {},
+                textColor: whiteColor,
+                title: "Add to cart"),
+          )
         ],
       ),
     );
